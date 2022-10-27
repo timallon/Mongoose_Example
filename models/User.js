ddb9c733d98d98f35dd55426154588d11cab45f0
Tim Allon <timallon@gmail.com>
2022-10-27 13:29:22 +0100

    iteration 2 complete, I think

--- models/User.js
+++ models/User.js
@@ -1,7 +1,32 @@
 // require mongoose and destructor Schema and model from it
 
 //Create a viking schema here
+
+const {Schema, model} = require("mongoose");
+
 //<================Schema=====================>
+
+const vikingSchema = new Schema ({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    familyName: {
+        type: String,
+        required: true,
+    },
+    strength: {
+        type: Number,
+    },
+    weapon: {
+        type: String,
+    },
+});
 
 //create a model with two arguments ====> (the name of the collection, schema or shape of data)
 //always make sure you export you model to use in other files
+
+const VikingModel = model("vikings", vikingSchema);
+module.exports = VikingModel;
+
+console.log("hello")
